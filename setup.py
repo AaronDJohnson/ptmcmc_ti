4c774def2f0202f45cd3c5cf997600e6bf6a2b4c
Aaron <Aaron9034@Hotmail.com>
2021-01-29 09:37:12 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ptmcmc_ti",
+    version="0.0.1",
+    author="Aaron Johnson",
+    author_email="johnsoad@uwm.edu",
+    description="Gelman-Rubin Rhat statistic for use with PTMCMCSampler output.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AaronDJohnson/ptmcmc_ti",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'jaxlib',
+        'jax',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
